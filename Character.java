179b612547b5eab057351e9539dcefeddd15df92
terryt14 <t.tucker@lancaster.ac.uk>
2017-02-15 16:08:21 +0000

    temporary change to be reverted

--- Character.java
+++ Character.java
@@ -2,6 +2,7 @@ public class Character
 {
   public static void main(String[] args)
   {
+    GameArena myArena = new GameArena(500,500);
     Rectangle body = new Rectangle(250.0,250.0,40.0,20.0,"RED");
     Rectangle top = new Rectangle(250.0,270.0,25.0,10.0,"RED");
     Ball leftWheel = new Ball(260.0,230.0,10,"GREY");
