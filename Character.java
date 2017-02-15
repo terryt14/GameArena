d096163e78a13c14f567a3f87a043b81435339dc
terryt14 <t.tucker@lancaster.ac.uk>
2017-02-15 16:04:06 +0000

    adding string args

--- Character.java
+++ Character.java
@@ -1,13 +1,16 @@
 public class Character
 {
-  Rectangle body = new Rectangle(250.0,250.0,40.0,20.0,"RED");
-  Rectangle top = new Rectangle(250.0,270.0,25.0,10.0,"RED");
-  Ball leftWheel = new Ball(260.0,230.0,10,"GREY");
-  Ball rightWheel = new Ball(240.0,230.0,10,"GREY");
-  myArena.addBall(leftWheel);
-  myArena.addBall(rightWheel);
-  myArena.addRectangle(body);
-  myArena.addRectangle(top);
-  double xPos;
-  double yPos;
+  public static void main(String[] args)
+  {
+    Rectangle body = new Rectangle(250.0,250.0,40.0,20.0,"RED");
+    Rectangle top = new Rectangle(250.0,270.0,25.0,10.0,"RED");
+    Ball leftWheel = new Ball(260.0,230.0,10,"GREY");
+    Ball rightWheel = new Ball(240.0,230.0,10,"GREY");
+    myArena.addBall(leftWheel);
+    myArena.addBall(rightWheel);
+    myArena.addRectangle(body);
+    myArena.addRectangle(top);
+    double xPos;
+    double yPos;
+  }
 }
