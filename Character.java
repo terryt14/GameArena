9bc9fe3d272c90829df523aef39edc719517bb16
JamesHymes <j.hymes@lancaster.ac.uk>
2017-02-15 16:00:56 +0000

    Add files via upload

--- Character.java
+++ Character.java
@@ -1,10 +1,9 @@
 public class Character
 {
-  GameArena myArena = new GameArena(500,500);
-  Rectangle body = new Rectangle(250.0, 250.0, 40.0, 20.0, "RED");
-  Rectangle top = new Rectangle(250.0, 270.0, 25.0, 10.0, "RED");
-  Ball leftWheel = new Ball(260.0, 230.0, 10, "GREY");
-  Ball rightWheel = new Ball(240.0, 230.0, 10, "GREY");
+  Rectangle body = new Rectangle(250.0,250.0,40.0,20.0,"RED");
+  Rectangle top = new Rectangle(250.0,270.0,25.0,10.0,"RED");
+  Ball leftWheel = new Ball(260.0,230.0,10,"GREY");
+  Ball rightWheel = new Ball(240.0,230.0,10,"GREY");
   myArena.addBall(leftWheel);
   myArena.addBall(rightWheel);
   myArena.addRectangle(body);
---
+++ Cones.java
@@ -0,0 +1,15 @@
+public class Cones
+{
+  Rectangle cone1 = new Rectangle(250.0,250.0,1.0,10.0,"ORANGE");
+  Rectangle cone2 = new Rectangle(250.0,251.0,1.0,8.0,"ORANGE");
+  Rectangle cone3 = new Rectangle(250.0,252.0,1.0,6.0,"WHITE");
+  Rectangle cone4 = new Rectangle(250.0,253.0,1.0,4.0,"WHITE");
+  Rectangle cone5 = new Rectangle(250.0,254.0,1.0,2.0,"ORANGE");
+  myArena.addRectangle(cone1);
+  myArena.addRectangle(cone2);
+  myArena.addRectangle(cone3);
+  myArena.addRectangle(cone4);
+  myArena.addRectangle(cone5);
+  double xPos;
+  double yPos;
+}
