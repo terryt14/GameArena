40d4161089a67772a5722b4da8805602330bf843
terryt14 <t.tucker@lancaster.ac.uk>
2017-02-22 15:52:15 +0000

    Update road.java

--- road.java
+++ road.java
@@ -1,5 +1,14 @@
 public class road
 {
+	public static void add(String[] args)
+	{
 	Rectangle topGrass = new Rectangle(250.0,40.0,500.0,80.0,"GREEN");
 	Rectangle bottomGrass = new Rectangle(250.0,460.0,500.0,80.0,"GREEN");
+	myArena.addRectangle(topGrass);
+	myArena.addRectangle(bottomGrass);
+	}
+	
+	public static void move(String[] args)
+	{
+	}
 }
